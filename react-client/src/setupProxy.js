8a057d176a0c4f363468702624c99e1ed5ae1a65
Tahshara Boochee <boocheet@gmail.com>
2020-04-28 09:12:05 -0700

    changed the proxy setup file

--- react-client/src/setupProxy.js
+++ react-client/src/setupProxy.js
@@ -1,10 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const proxy = () => {createProxyMiddleware({
+const proxy = () => createProxyMiddleware({
   target: 'http://localhost:3001',
   changeOrigin: true,
   logLevel: 'debug'
   })
-}
+
 module.exports = function(app) {
   app.use('/login', proxy());
   app.use('/callback', proxy());
