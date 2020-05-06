e05842939248ca094857c1635da3d2b53f7fd7c1
Tahshara Boochee <boocheet@gmail.com>
2020-05-06 11:32:34 -0700

    added login component with logo

--- react-client/src/components/login/Login.js
+++ react-client/src/components/login/Login.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './login.css';
+import '../logo/Mood Lifter-logo-white.png'
 
 const Login = (props) => {
     const {token} = props
     return (
-        <nav style={{ display: 'flex', justifyContent: 'center' }}>
+        <nav className='container'>
+            <div className='item' >
+            {/* use require to load an img https://stackoverflow.com/questions/34582405/react-wont-load-local-images */}
+                <img style={{width: 200, height: 200}} src= {require('../logo/Mood Lifter-logo-white.png')}></img>
+            </div>
+            <div className='item'>
             <button className="f3 link dim white underline pa3 pointer btn btn--loginApp-link">
                {token ? <a href="/logout">{'Logout of Spotify'}</a> : <a href="/login">{'Login to Spotify'}</a>}
             </button>
+            </div>
         </nav>
     );
 };
