22d63446f56626ce0b66a914fccd6d6f04b68c9a
Tahshara Boochee <boocheet@gmail.com>
2020-05-06 11:31:43 -0700

    change background color

--- react-client/src/components/ColorChanger/ColorChanger.js
+++ react-client/src/components/ColorChanger/ColorChanger.js
@@ -1,43 +1,47 @@
 import React, { Component } from 'react';
 import './ColorChanger.css';
 class ColorChanger extends Component {
- componentDidMount(){
-    // const arrColor = ["#45c1bf", "#f0593e", "#aeacd4", "#bdd630", "#4479bd", "#f5b11e"];
-    const arrColor = ['#FF0000','#FF2A00','#FF5500','#FF8000','#FF002A','#FF8000','#FFAA00', "#45c1bf", "#f0593e", "#aeacd4", "#bdd630", "#4479bd", "#f5b11e"];
-    //   //random new color
-    function GetNewColor() {
-      // let index = 0;
-      // console.log(index)
-      // if (index === arrColor.length){
-      //   index = 0
-      // } else {
-      //   index++
-      //   return arrColor[index];
-      // }
-        let index = Math.floor((Math.random() * 12) + 1);
-        // let index = Math.floor((Math.random() * 5) + 1);  
-        return arrColor[index] 
-        
-      }
-      
-      // set new color
-    function SetNewColor(color) {
-        document.body.style.background = color;
-      }
-      // on document load function start
-      (function() {
-        let colorSelected = arrColor[0]
-        // let colorSelected = GetNewColor();
-        SetNewColor(colorSelected);
-      })();
-    //change color timer
-    window.setInterval(function() {
-      // SetNewColor(color)
-      let colorSelected = GetNewColor();
-      console.log(colorSelected)
-      SetNewColor(colorSelected);
-    }, 2000);
- }
+    componentDidMount() {
+        // const arrColor = ["#45c1bf", "#f0593e", "#aeacd4", "#bdd630", "#4479bd", "#f5b11e"];
+        const arrColor = [
+            '#FF0000',
+            '#FF2A00',
+            '#FF5500',
+            '#FF8000',
+            '#FF002A',
+            '#FF8000',
+            '#FFAA00',
+            '#45c1bf',
+            '#f0593e',
+            '#aeacd4',
+            '#bdd630',
+            '#4479bd',
+            '#f5b11e',
+        ];
+        //   //random new color
+        function GetNewColor() {
+            let index = Math.floor(Math.random() * 12 + 1);
+            // let index = Math.floor((Math.random() * 5) + 1);
+            return arrColor[index];
+        }
+
+        // set new color
+        function SetNewColor(color) {
+            document.body.style.background = color;
+        }
+        // on document load function start
+        (function () {
+            let colorSelected = GetNewColor();
+            SetNewColor(colorSelected);
+        })();
+        //change color timer
+        window.setInterval(function () {
+            // SetNewColor(color)
+            let colorSelected = GetNewColor();
+            // console.log(colorSelected)
+            SetNewColor(colorSelected);
+        }, 2000);
+    }
     render() {
         return (
             <div>
