f475d352f7f8af2bcad247932e361f028b758f62
Tahshara Boochee <boocheet@gmail.com>
2020-06-03 11:09:53 -0700

    removed mood from player and refactored player

--- react-client/src/components/player/Player.js
+++ react-client/src/components/player/Player.js
@@ -1,32 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { css } from '@emotion/core';
-import DotLoader from 'react-spinners/DotLoader';
-import Moods from '../moods/Moods';
+// import Moods from '../moods/Moods';
 import PlayButtons from '../playButtons/PlayButtons';
 import './Player.css';
 import { Card, CardImg, CardBody, CardHeader, CardTitle, CardFooter } from 'reactstrap';
 
-const override = css`
-    display: block;
-    margin: 0 auto;
-    border-color: black;
-`;
 const Player = (props) => {
-    const [onPlayListClick, setOnPlayListClick] = useState(false);
     const [onBackClick, setOnBackClick] = useState(false);
-    useEffect(() => {
-        if (!props.state.playing) {
-            setOnPlayListClick(!onPlayListClick);
-        }
-    }, []);
-
-    const playlistPlaying = () => {
-        setOnPlayListClick(!onPlayListClick);
-    };
     const backClick = () => {
         setOnBackClick(!onBackClick);
-        setOnPlayListClick(!onPlayListClick);
         props.onPlayClick();
+        props.onPlaylistClick();
     };
 
     const { userInfo, artistName, songName, backgroundImage, usersPlaylists, token, loading, playing } = props.state;
@@ -39,46 +22,28 @@ const Player = (props) => {
                     color: 'white',
                 }}
             >
-                {loading ? (
-                    <div>
-                        <DotLoader css={override} size={150} color={'#123abc'} loading={loading} /> 
-                        <p className='f3' style={{
-                            color: 'white'
-                        }}> {'Please wait while your playlists load'}</p>
-                    </div>
-                ) : !onPlayListClick ? (
-                    <div>
-                        <CardHeader>
-                            <button className="btn btn--playlist" onClick={backClick}>
-                                Click a different Mood
-                            </button>
-                        </CardHeader>
-                        <div className="pa3"></div>
-                        <CardTitle>
-                            {songName} by: {artistName}
-                        </CardTitle>
-                        <CardBody>
-                            <CardImg className="" src={backgroundImage} alt="album cover" />
-                        </CardBody>
-                        <CardFooter>
-                            <PlayButtons
-                                state={props.state}
-                                onPrevClick={props.onPrevClick}
-                                onNextClick={props.onNextClick}
-                                onPlayClick={props.onPlayClick}
-                            />
-                        </CardFooter>
-                    </div>
-                ) : (
-                    <Moods
-                        userName={userInfo.display_name}
-                        userId={userInfo.id}
-                        playlists={usersPlaylists}
-                        playlistPlaying={playlistPlaying}
-                        token={token}
-                        playing={playing}
-                    />
-                )}
+                <div>
+                    <CardHeader>
+                        <button className="btn btn--playlist" onClick={backClick}>
+                            Click a different Mood
+                        </button>
+                    </CardHeader>
+                    <div className="pa3"></div>
+                    <CardTitle>
+                        {songName} by: {artistName}
+                    </CardTitle>
+                    <CardBody>
+                        <CardImg className="" src={backgroundImage} alt="album cover" />
+                    </CardBody>
+                    <CardFooter>
+                        <PlayButtons
+                            state={props.state}
+                            onPrevClick={props.onPrevClick}
+                            onNextClick={props.onNextClick}
+                            onPlayClick={props.onPlayClick}
+                        />
+                    </CardFooter>
+                </div>
             </Card>
         </div>
     );
