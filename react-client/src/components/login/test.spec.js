5120338123459d330da28045c18b4ec03e0e29b4
Tahshara Boochee <boocheet@gmail.com>
2020-06-17 06:54:47 -0700

    all test passing

--- react-client/src/components/login/test.spec.js
+++ react-client/src/components/login/test.spec.js
@@ -33,27 +33,6 @@ describe('Login Component', () => {
             const component = findByTestAtr(wrapper, 'loginComponent');
             expect(component.length).toBe(1);
         });
-        it('Test click event', () => {
-            const testState = { width: 10, height: 20 };
-            // const wrapper = shallow((
-            //   <Login
-            //     width={testState.width}
-            //     height={testState.height}
-            //     onChange={(e) => {
-            //       testState[e.target.name] = e.target.value;
-            //     }}
-            //   />
-            // ));
-            console.log(wrapper.find('button'), wrapper.find('a').at(0).prop('value'))
-            expect(wrapper.find('a').at(0).prop('value')).toEqual(10);
-            expect(wrapper.find('a').at(1).prop('value')).toEqual(20);
-            wrapper.find('a').at(0).simulate('change', { target: { name: 'width', value: 50 } });
-            wrapper.find('a').at(1).simulate('change', { target: { name: 'height', value: 70 } });
-            expect(testState.width).toEqual(50);
-            expect(testState.height).toEqual(70);
-            
-        
-        })
     })
     describe('Have NO props', () => {
         let wrapper; 
